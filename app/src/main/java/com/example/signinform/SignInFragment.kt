48ce66c4ee3520ed2e702f55f2cf1cd5e95f5ac6
Anand Limbu <graphdroid.190@gmail.com>
2020-10-24 21:27:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/signinform/SignInFragment.kt
@@ -0,0 +1,27 @@
+package com.example.signinform
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_sign_in.view.*
+
+class SignInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_sign_in, container, false)
+
+        view.signUpTxt.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_signInFragment_to_createAccountFragment)
+        }
+
+        return view
+    }
+
+}
